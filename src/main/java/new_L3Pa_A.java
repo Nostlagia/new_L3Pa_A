3573bad9e790a0bfbe2240ca952e6cdc36d63233
Nostalgia <1159155672@qq.com>
2020-08-18 17:42:43 +0800

    Initial commit

---
+++ src/main/java/new_L3Pa_A.java
@@ -0,0 +1,15 @@
+public class new_L3Pa_A {
+    public static void main(String[] args) {
+        B b = new B();
+        b.m1();
+
+        new_L3Pa_C new_l3Pa_c = new new_L3Pa_C();
+        new_l3Pa_c.cCall();
+
+        new_L3Pa_E new_l3Pa_e = new new_L3Pa_E();
+        new_l3Pa_e.e();
+
+        new_L3Pa_F new_l3Pa_f = new new_L3Pa_F();
+        new_l3Pa_f.f();
+    }
+}
